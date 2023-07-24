307079a4851bf1e813f9596d1f2ca153a6c2fa02
jhigherevature <71463449+jhigherevature@users.noreply.github.com>
2023-07-24 11:31:19 -0600

    Updated instructions for ParentObjectClassMethods

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -1,4 +1,15 @@
 /**
+ * Your goal for this activity is to provide an overriden implementation of the 'equals' method and the 'toString'
+ * method. Please note that these methods are automatically inherited by all classes in Java from the Object class.
+ * 
+ * For the equals() method, two objects will be considered equal if the fields 'a' and 'b' are equal in both objects. 
+ * The equals() should take in a parameter that is another SampleClass object, and access its version of the fields 
+ * 'a' and 'b'. Return true if the values of 'a' and 'b' are equal in both objects, and false otherwise.
+ *
+ * For the toString() method, the test cases will check if the String returned by the toString method contains 
+ * information about the fields of this object. Essentially, you just need to append them to the String that is 
+ * returned by toString. The string may be formatted in any way.
+ *
  * The test cases for this class use methods called .equals which aren't written anywhere in this class. How?
  * All objects have a method called .equals and a method called toString - they are inherited from the Object class,
  * which is actually the parent class of all Java classes. Classes are able to 'extend' other classes, which will
@@ -13,28 +24,20 @@
  * the OOP principle of polymorphism, specifically runtime polymorphism (since there are situations where the exact
  * class of an object isn't known until runtime).
  *
- * for instance, the code:
+ * for instance, the following code:
  * public boolean equals(SampleClass other){
  *    //code here
  * }
  * Will override the default equals method when iem1.equals(iem2); is called. That equals method can be used to compare
  * 'this' object with another SampleClass named 'other'.
- * whereas the code:
+ *
+ * whereas this code:
  * public String toString(){
  *   //code here
  * }
  * Will override the default toString() method. The String that is returned is the String representation of this object.
  *
- * Overriding equals and toString will be the task for this challenge:
- *
- * For .equals, two objects will be considered equal if a and b are equal in both objects. .equals should take in a
- * parameter that is another SampleClass object, and access its version of a and b. Return true if the values
- * of a and b are equal in both objects, and false otherwise.
- *
- * For .toString, the test cases will check if the String returned by the toString method contains information about
- * the fields of this object. Essentially, you just need to append them to the String that is returned by toString.
- * The string may be formatted in any way.
- *
+ * Please keep the following in mind:
  * .equals addendum: Why can't we just compare objects with ==? == is referred to as a shallow comparison - it only
  * compares the 'address' of an object in memory. Two objects can have equivalent values of a,b, and c but == could
  * resolve to false because the equivalent object is located in a different place in memory. == will resolve to true
